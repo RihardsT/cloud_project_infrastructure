4ada42b19645d322fa57e5c6f8be8782acbb0023
Rihards Teteris <richitislv@gmail.com>
2017-03-02 23:00:19 +0200

    Update command list, and compose file
    Remove Postgres ARM Debian variant as i'm not going to look into that any further

--- Commands.sh
+++ Commands.sh
@@ -1,6 +1,12 @@
-sudo mkdir /srv/blog_app
-sudo chown USER:USER /srv/blog_app
+sudo mkdir /srv/blog
+sudo chown -R blogadmin:blogadmin /srv/blog
 git clone ## clone this repo
 cd ## cd where needed: DockerGunicornARM
+git submodule init
+git submodule update
 docker login # login to your docker contianter registry
+
+apt-get install python3-pip
+pip3 install --upgrade pip
+pip3 install --upgrade docker-compose
 docker-compose up -d
